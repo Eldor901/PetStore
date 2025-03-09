81028af4e0f5db4e3f8280af2f9be21e611b1207
Eldor <eldor9012545@gmail.com>
2025-03-10 01:51:20 +0500

    Set up Swagger for code generation

--- rest-server/src/server.ts
+++ rest-server/src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { petsRouter } from "./routes/petStore.routes";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use("/api/pets", petsRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`REST Server running on port ${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 });
